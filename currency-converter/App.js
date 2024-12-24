3a83ac5c61e0e2a658aa770236277a1804167ec2
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-12-24 08:23:39 +0530

    Modify App.js

--- currency-converter/App.js
+++ currency-converter/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView>
+          <View style={styles.container}>
+            <Home />
+            <StatusBar style="auto" />
+          </View>
+        </SafeAreaView>
+    </PaperProvider>
   );
 }
 
