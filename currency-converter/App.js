d055ea4068f28f377a5521d7c4fc0480822cbe5b
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2024-12-24 09:32:51 +0530

    add more styles to App.js

--- currency-converter/App.js
+++ currency-converter/App.js
@@ -1,22 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Home from './components/Home'
+import Home from './components/Home.js';
+
 export default function App() {
   return (
     <PaperProvider>
-      <SafeAreaView>
-          <View style={styles.container}>
-            <Home/>
-            <StatusBar style="auto" />
-          </View>
-        </SafeAreaView>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <Home />
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, 
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
